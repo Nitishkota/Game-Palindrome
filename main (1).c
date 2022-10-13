ed2378fb18c2c83b358fc44090e7cc7e3030f91f
Nitishkota <55479057+Nitishkota@users.noreply.github.com>
2022-10-13 09:44:24 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    
+    char name[23];
+    char age[2];
+    char course[20];
+    
+    printf("Enter the n1");
+    scanf("%s", name);
+    printf("Enter the n2");
+    scanf("%s", age);
+    printf("Enter the n3");
+    scanf("%s", course);
+    
+    printf("My name is %s/n", name);
+    printf(" and And my age is %s/n", age);
+    printf("And my course at UNM is %s/n", course);
+    
+    
+    
+    
+
+    return 0;
+}
+
+
+
